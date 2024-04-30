7f75116e264487f0febb4a16cbc5a3a60d0d7fe0
Maksymilian Grabka <m@mgrabka.com>
2024-04-30 21:27:08 +0200

    Add stevearc/oil.nvim

---
+++ lua/config/lazy/oil.lua
@@ -0,0 +1,12 @@
+return {
+  'stevearc/oil.nvim',
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function(config)
+    require('oil').setup({
+      default_file_explorer = true,
+    })
+
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
+}
